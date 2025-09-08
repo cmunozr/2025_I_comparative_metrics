5429b03bfe743dbf7a1fdb08701363a2f521042f
Carlos Munoz <cmunozbiol@gmail.com>
2025-09-09 01:36:16 +0200

    luke server was updated, needed changes implemented to download. Now using a master dictionary of covariates

--- R/_utilities_download_shultz_covariates.R
+++ R/_utilities_download_shultz_covariates.R
@@ -5,6 +5,10 @@ library(xml2)
 library(fs)
 options(timeout = 1000)
 
+dict_covar <- read.csv(file.path("data", "covariates", "dictionary_covariates.csv"), sep = ";")
+
+years <- c(2009, 2011, 2013, 2015, 2017, 2019, 2021)
+
 #-----------------
 # Download TREE HEIGHT
 # https://www.sciencedirect.com/science/article/pii/S0034425723003486?via%3Dihub
@@ -83,8 +87,6 @@ get_all_disk_space_windows <- function() {
 # Download from metsakeskus: LATVUS model
 
 preferred_disk_order <- c("D:", "E:")
-
-years <- c(2009, 2011, 2013, 2015, 2017, 2019, 2021)
 
 base_url <- "https://avoin.metsakeskus.fi/aineistot/Latvusmalli/Karttalehti/"
 
@@ -207,115 +209,49 @@ for(i in 1:length(folder_links)){
 # http://www.nic.funet.fi/index/geodata/luke/vmi/
 # Average stand diameter, Average stand length, Canopy cover broadleaves
 # Canopy cover whole stand, Stand age, Stand basal area, Total wood volume
-# Spruce volume, pine volume, birch volume, 
-# first run _utilities_download_shultz_covariates.py
-# to activate environment in vscode: C:/.compMetrics/Scripts/Activate.ps1
-# extract list of links from mail
-# run next
+# Spruce volume, pine volume, birch volume, broadleaves volume
 
-luke_urls <- readClipboard() #comming from the mail
-prefered_disk <- "E:"
+dict_luke <- dict_covar |> 
+  filter(set == "luke", to_download == 1, downloaded == 0)
 
-for (i in 11:length(luke_urls)) {
-  #i <- 10
-  url_i <- luke_urls[i]
-  
-  webpage <- read_html(url_i)
-  
-  nm <- webpage |>
-    html_node("table") |> 
-    paste(concatenate = ",") |> 
-    str_split(pattern = "<td>|</td>", simplify = T)
-  nm <- (nm[2]) |> 
-    str_replace_all(pattern = " |/|-", replacement = "_")
+base_url <- "http://www.nic.funet.fi/index/geodata/luke/vmi/"
 
-  yr_ <- str_extract_all(nm, pattern = "\\d")[[1]]
-  
-  if(length(yr_) == 5){
-    yr_ <- yr_[-5]
-  }
-  
-  yr_ <- yr_ |> 
-    paste(collapse = "")
-  
-  links <- webpage |> 
-    html_nodes("a") |>
-    html_attr("href")
-  
-  download_dir <- file.path(prefered_disk, "luke", yr_)
-  
-  dir.create(download_dir, showWarnings = F, recursive = T)
+prefered_disk <- "D:"
 
-  # Define the full path for saving the file
-  dest_file <- file.path(download_dir, nm)
+for(i in 1:nrow(dict_luke)){
+  # i <- 1
+  var_name <- dict_luke[i, "file_name"]
   
-  dest_file_zip <- paste0(dest_file, "_batch1.zip")
+  url_years <- paste0(base_url, years)
   
-  if (file.exists(dest_file_zip)) {
-    dest_file_zip <- paste0(dest_file, "_batch2.zip")
-  }
-  
-  tryCatch({
-    download.file(links, destfile = dest_file_zip, mode = "wb") # 'wb' for binary files
-  }, error = function(e) {
-    message(paste0("Error downloading ", filename, ": ", e$message))
-  })
-}
-
-#------
-
-library(fs)
-
-base_directory <- "E:/luke/"
-
-# Get a list of all main folders (FolderX, e.g., Folder1, Folder2)
-main_folders <- dir_ls(base_directory, type = "directory")
-
-# Loop through each main folder
-for (folder_path in main_folders) {
-  #folder_path <- main_folders[1]
-  folder_name <- path_file(folder_path)
-  
-  zip_files_in_folder <- dir_ls(folder_path, glob = "*.zip")
-  
-  for (zip_file_path in zip_files_in_folder) {
-    # zip_file_path <- zip_files_in_folder[1]
-    zip_file_name <- path_file(zip_file_path)
+  for(a in 1:length(url_years)){
+    # a <- 2
+    webpage <- read_html(url_years[a])
     
-    # Create a temporary directory for extraction
-    temp_extract_dir <- path(base_directory, paste0("temp_extract_", Sys.time() |> as.integer(), "_", sample(1:1000, 1)))
-    dir_create(temp_extract_dir)
+    links <- webpage |> 
+      html_nodes("a") |>
+      html_attr("href") |> 
+      str_subset(".tif")
     
-    # Attempt to unzip the current zip file
-    unzip_success <- tryCatch({
-      unzip(zip_file_path, exdir = temp_extract_dir, junkpaths = FALSE)
-      TRUE
+    var_name_ <- str_replace(string = var_name, pattern = "(.*)_([^_]*)$", replacement = "\\1\\2")
+    
+    link_detected <- links[str_detect(links, pattern = paste0("^", var_name_, "(\\.|_)"))]
+    
+    link_to_download <- paste0(url_years[a], "/", link_detected)
+    
+    download_dir <- file.path(prefered_disk, "luke_", years[a])
+    
+    dir.create(download_dir, showWarnings = F, recursive = T)
+    
+    # Define the full path for saving the file
+    dest_file <- file.path(download_dir, paste0(var_name_, ".tif"))
+    
+    tryCatch({
+      download.file(link_to_download, destfile = dest_file, mode = "wb") # 'wb' for binary files
     }, error = function(e) {
-      message(paste0("    ERROR unzipping '", zip_file_name, "': ", e$message, "\n"))
-      FALSE
+      message(paste0("Error downloading ", filename, ": ", e$message))
     })
     
-    if (!unzip_success) {
-      dir_delete(temp_extract_dir) # Clean up failed temp dir
-      next
-    }else{
-      subfolder <- dir_ls(temp_extract_dir, type = "directory")
-      
-      
-      # --- Process nested zip files ---
-      nested_zip_files <- dir_ls(subfolder, glob = "*.zip")
-      
-      if (length(nested_zip_files) > 0) {
-        for (nested_zip in nested_zip_files) {
-          target_path <- path(folder_path, path_file(nested_zip))
-          file_move(nested_zip, target_path)
-        }
-      }
-      
-      # --- Deleting ---
-      
-      file_delete(zip_file_path)
-      dir_delete(temp_extract_dir)
-    }
   }
+  
 }
